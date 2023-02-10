b5afe8392e85cf5f3d7844d24d6fa66edc1b3afb
Emanuel Massafera <emanuel301@live.com>
2023-02-10 08:37:44 -0300

    feat: add route to list tasks

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,21 @@
-export const routes = []
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', {
+        title: search,
+        description: search
+      })
+
+      return res.writeHead(200).end(JSON.stringify(tasks))
+    }
+  }
+]
